cfe9f0ff4487668600c7f2ecc61ba9a25ce6158d
engels.fabian <engels.fabian@db254056-2ad5-e4aa-e4d8-bb6a4be5ecbc>
2013-01-19 19:27:47 +0000

    added: set of clients

--- svs-chat-server/src/main/java/de/bhtberlin/svschatserver/Server.java
+++ svs-chat-server/src/main/java/de/bhtberlin/svschatserver/Server.java
@@ -5,9 +5,12 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Hello world!
+ * SVS UDP Client Chat v0.01
+ * Sven Höche, Fabian Engels
  *
  */
 public class Server 
@@ -17,20 +20,27 @@ public class Server
         DatagramSocket sendSocket;
         DatagramSocket serverSocket;
         DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+        Set <InetAddress> clients = new HashSet();
         try{
             serverSocket = new DatagramSocket(9600);
             while(true){
                 serverSocket.receive(packet);
                 System.out.println("Received from IP:" + packet.getAddress());
                 byte[] buffer = packet.getData();
-                String text = new String(buffer,"UTF8");
-                System.out.println("Received Data: " + text);
-                InetAddress address = packet.getAddress();
-                packet.setAddress(address);
-                packet.setPort(9602);
+                String text = new String(buffer, "UTF8");
                 
+                System.out.println("Received Data: " + text);
+                InetAddress sourceAddr = packet.getAddress();
+                clients.add(sourceAddr);
+                if(text.equalsIgnoreCase("close")){
+                  clients.remove(sourceAddr);  
+                }
+                packet.setPort(9602);
                 sendSocket = new DatagramSocket(9601);
-                sendSocket.send(packet);
+                for(InetAddress iaddr: clients){
+                    packet.setAddress(iaddr);
+                    sendSocket.send(packet);
+                }
                 sendSocket.close();
                 System.out.println("Packet got send!");
             }
