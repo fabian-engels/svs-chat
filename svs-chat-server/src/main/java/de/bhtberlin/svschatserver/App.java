f36c1428470c9737d6fa479a487c01c9bbe71c81
engels.fabian <engels.fabian@db254056-2ad5-e4aa-e4d8-bb6a4be5ecbc>
2013-01-19 16:50:06 +0000

    init

---
+++ svs-chat-server/src/main/java/de/bhtberlin/svschatserver/App.java
@@ -0,0 +1,13 @@
+package de.bhtberlin.svschatserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
